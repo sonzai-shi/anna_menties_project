4d7a70edf2867b042a0fbd3ce5f15606a753ef72
Anna <sonzai_shi@inbox.ru>
2026-09-02 01:38:42 +0300

    Удалила лишние миграции, сделала одну Перенесла  в отдельный метод в application подключение роутеров

--- src/application.py
+++ src/application.py
@@ -1,11 +1,19 @@
 from email.utils import rfc2231_continuation
-
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
-
 from src.router.healthcheck import router as healthcheck_router
-from src.router.v1 import v1_router
+
+from src.router.v1.books_authors_router import router as ba_router
+from src.router.v1.countries_capital_router import router as cc_router
+from src.router.v1.courses_students_router import router as cs_router
+
+def get_v1_router() -> APIRouter:
+    v1_router = APIRouter()
+    v1_router.include_router(cs_router, prefix="/v1")
+    v1_router.include_router(ba_router, prefix="/v1")
+    v1_router.include_router(cc_router, prefix="/v1")
+    return v1_router
 
 
 def get_app() -> FastAPI:
@@ -24,5 +32,5 @@ def get_app() -> FastAPI:
     )
 
     app.include_router(healthcheck_router)
-    app.include_router(v1_router)
+    app.include_router(get_v1_router())
     return app
