0e68278edaec218652a3da9785c10bf2f6b8b76a
Anna <sonzai_shi@inbox.ru>
2026-09-18 14:45:38 +0300

    Сделала самописные ошибки

--- src/schemas/base.py
+++ src/schemas/base.py
@@ -1,13 +1,14 @@
 from fastapi import HTTPException, status
+import src.exceptions.schema_exception as se
 
 
 def letter_only_str(value: str) -> str | None:
     if value is None:
         return value
     if not value.replace(' ', '').isalpha():
-        raise HTTPException(
+        raise se.StringContainsNonLettersException(
             status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
-            detail='Use only letters.'
+            detail=f'The string: {value} must contain only letters.'
         )
     return value
 
@@ -17,9 +18,9 @@ def letter_only_list(value: list[str]) -> list[str] | None:
         return value
     for genre in value:
         if not genre.replace(' ', '').isalpha():
-            raise HTTPException(
+            raise se.ListContainsNonLettersException(
                 status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
-                detail='Use only letters.'
+                detail=f'The genre: {genre} must consist only of letters.'
             )
     return value
 
@@ -28,7 +29,7 @@ def not_empty_str(value: str) -> str | None:
     if value is None:
         return value
     if not value.replace(' ', ''):
-        raise HTTPException(
+        raise se.EmptyStringException(
             status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
             detail=f'The string cannot be empty.'
         )
@@ -39,11 +40,8 @@ def not_empty_list(value: list) -> list | None:
     if value is None:
         return value
     if not value:
-        raise HTTPException(
+        raise se.EmptyListException(
             status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
             detail=f'The list must not be empty.'
         )
     return value
-
-
-
--- src/service/course_service.py
+++ src/service/course_service.py
@@ -1,5 +1,5 @@
 from uuid import UUID
-from fastapi import HTTPException, status
+from fastapi import status
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.mappers import course_mapper
 from src.models.courses import CourseModel
@@ -9,48 +9,43 @@ from src.schemas.course import (
     CourseSchemaUpdate,
 )
 from src.repositories.repository import Repository
+from src.exceptions.service_exception import ObjectNotFoundException
 
 
 class CourseService:
     def __init__(self, session: AsyncSession):
         self.course_repo = Repository(session)
 
-
     async def create_course(self, course_data: CourseSchemaCreate):
         course = course_mapper.to_model(course_data)
         result = await self.course_repo.create(course)
         return CourseSchemaResponse.model_validate(result)
 
-
     async def read_course(self, course_id: UUID):
         result = await self.find_course(course_id)
         return CourseSchemaResponse.model_validate(result)
 
-
     async def read_courses(self, offset: int, limit: int):
         result = await self.course_repo.find_many(CourseModel, 'students', offset, limit)
         return course_mapper.to_pagination(courses=result, offset=offset, limit=limit)
-
 
     async def update_course(self, course_id: UUID, data: CourseSchemaUpdate):
         course = await self.find_course(course_id)
         course_mapper.update_course(course, data)
         return CourseSchemaResponse.model_validate(course)
 
-
     async def delete_course(self, course_id: UUID):
         result = await self.find_course(course_id)
         result.is_deleted = True
         for student in result.students:
             student.is_deleted = True
-        return 'The course has been removed.'
-
+        return f'Course with ID: {course_id} has been removed.'
 
     async def find_course(self, course_id: UUID):
         result = await self.course_repo.find_one(CourseModel, course_id, 'students')
         if result is None:
-            raise HTTPException(
+            raise ObjectNotFoundException(
                 status_code=status.HTTP_404_NOT_FOUND,
-                detail='Course not found.'
+                detail=f'Course with ID: {course_id} not found.'
             )
         return result
